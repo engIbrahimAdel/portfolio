0cc156a0ccc442feb94c236490906ac90965c4e0
engIbrahimAdel <ibrahim.zahra91@eng1.cu.edu.eg>
2018-04-16 08:19:05 +0200

    adding url for media files to show at server

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#must add this conf to use settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+  #this to make a url at the admin page to show media
